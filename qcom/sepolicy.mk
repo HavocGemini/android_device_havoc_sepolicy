0233ab5f7392b4ef670949baa8d390bbb0549a65
Kazakov Stepan <ksrt12group@gmail.com>
2019-01-18 21:53:20 +0300

    update sepolicy for gemini
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,6 +3,9 @@
 # that inherit from Havoc
 #
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/havoc/sepolicy/qcom/private
+
 BOARD_SEPOLICY_DIRS += \
     device/havoc/sepolicy/qcom/common \
     device/havoc/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
